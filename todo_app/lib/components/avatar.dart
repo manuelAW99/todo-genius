b0f7d7a14a2609ba457da30d46fb70171c5920bf
Manuel Vilas Valiente <mvilasvaliente@gmail.com>
2024-10-22 21:07:08 -0400

    Refactor Avatar widget to display a circular image and add a fallback text when no image is available

--- todo_app/lib/components/avatar.dart
+++ todo_app/lib/components/avatar.dart
@@ -28,17 +28,22 @@ class _AvatarState extends State<Avatar> {
           Container(
             width: 150,
             height: 150,
-            color: Colors.grey,
+            decoration: const BoxDecoration(
+              color: Colors.grey,
+              shape: BoxShape.circle,
+            ),
             child: const Center(
               child: Text('No Image'),
             ),
           )
         else
-          Image.network(
-            widget.imageUrl!,
-            width: 150,
-            height: 150,
-            fit: BoxFit.cover,
+          ClipOval(
+            child: Image.network(
+              widget.imageUrl!,
+              width: 150,
+              height: 150,
+              fit: BoxFit.cover,
+            ),
           ),
         ElevatedButton(
           onPressed: _isLoading ? null : _upload,
